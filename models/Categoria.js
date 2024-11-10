de8c26895dc9a9a27c4c7296eb27a4ac2a779a11
guilhermedel <glhrmgithub@gmail.com>
2024-11-10 16:17:18 -0300

    att

--- models/Categoria.js
+++ models/Categoria.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const CategoriaSchema = new mongoose.Schema({
   categoryName: { type: String, required: true },
   categoryColor: { type: String,required: true},
-  revenueValue: {type:Number,required:true},
+  revenueValue: {type:Number},
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }
 });
 
