75b21eaec187351d88eee6091ab166ff62ce3131
guilhermedel <glhrmgithub@gmail.com>
2024-11-10 17:32:10 -0300

    att type number

--- models/ContaBancaria.js
+++ models/ContaBancaria.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const ContaBancariaSchema = new mongoose.Schema({
   accountBankingName: { type: String, required: true },
-  accountBalance: { type: String, required: true },
+  accountBalance: { type: Number, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }
 });
 
